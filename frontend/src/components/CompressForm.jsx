756d2f0eb25d43faee2a0ee771bd542648699e6e
Mahallis <mahallis@fatuhdvvpo.local>
2024-11-20 12:58:37 +0000

    fix: configure network connections

--- frontend/src/components/CompressForm.jsx
+++ frontend/src/components/CompressForm.jsx
@@ -23,7 +23,7 @@ export default function CompressForm() {
     const formData = new FormData(event.target);
 
     try {
-      const response = await fetch("http://backend:8000/compress/", {
+      const response = await fetch("/compress/", {
         method: "POST",
         body: formData,
       });
